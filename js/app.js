554f7c80d2faa9ff2306e35f50e75be3538ad83e
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-01-06 10:51:42 +0200

    implement oop approach to the game

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ class Game {
 
 	}
 
-	_makeCards() {
+	makeCards() {
 		// make single card of specified type in container
 		let makeCard = (type) => {
 			let card = document.createElement( 'div' );
@@ -33,101 +33,64 @@ class Game {
 	}
 
 	init() {
-		_makeCards();
+		this.makeCards();
 		this.cards = document.querySelectorAll('.card');
+		this.addClickHandlers(this.cards);
+	}
+
+	pickCard(card) {
+		card.classList.add('picked');
+		this.pickedCards.push(card);
+	}
+
+	closeCards() {
+		this.pickedCards.forEach(function(card) {
+			card.classList.remove('picked');
+		});
+		this.pickedCards.length = 0;
+	};
+
+	checkWin() {
+		if (this.hiddenPairsCount === 8) {
+			setTimeout(function() {
+				alert('You won!');
+			}, 300);
+		}
+	}
+
+	hidePickedCards(type) {
+		this.pickedCards.forEach(function(card) {
+			card.classList.add( 'hidden' );
+		});
+		this.pickedCards.length = 0;
+		this.hiddenPairsCount++;
+		this.checkWin();
+	};
+
+	getType(card) {
+		return card.getAttribute("class").slice(5, 11);
+	}
+
+	addClickHandlers(cards) {
+		cards.forEach( card => {
+			card.addEventListener('click', () => {
+				if (this.pickedCards.length === 0) {
+					this.pickCard(card);
+				} else if (this.pickedCards.length === 1) {
+					this.pickCard(card);
+					if (this.pickedCards[0] === this.pickedCards[1]) {
+						this.closeCards();
+					} else if ( this.getType(this.pickedCards[0]) === this.getType(this.pickedCards[1]) && this.pickedCards[0] !== this.pickedCards[1]) {
+						setTimeout(this.hidePickedCards.bind(this), 800);
+					} else {
+						setTimeout(this.closeCards.bind(this), 800);
+					}
+				}
+			});
+		});
 	}
 
 }
 
 const game = new Game;
-
-console.log(game);
-
-
-// make html markup by JS
-
-// makes an array with 16 card classes of 8 different types (from type-0 to type-7)
-var cardClasses = Array(16).fill('type-').map(function(item, i) {
-	if (i > 7) return item + (i - 8);
-	return item + i;
-});
-
-// shuffle it
-cardClasses = shuffle(cardClasses);
-
-// make single card of specified type in container 
-var container = document.querySelector('.container');
-
-var makeCard = function(type) {
-	var card = document.createElement( 'div' );
-	card.setAttribute( 'class', 'card ' + type );
-	card.innerHTML = '<div class="flip-container"><div class="flipper"><div class="front"></div><div class="back"></div></div></div>';
-	container.appendChild( card );
-};
-
-// populate containder with cards
-cardClasses.forEach(function(type) {
-	makeCard(type);
-});
-
-// game
-var pickedCards = [];
-var hiddenPairsCount = 0;
-var cards = document.querySelectorAll('.card');
-
-var pickCard = function(card) {
-	card.classList.add('picked');
-	pickedCards.push(card);
-}
-
-var closeCards = function() {
-	pickedCards.forEach(function(card) {
-		card.classList.remove('picked');
-	});
-	pickedCards.length = 0;
-};
-
-var checkWin = function() {
-	if (hiddenPairsCount === 8) {
-		setTimeout(function() {
-			alert('You won!');
-		}, 300);
-	}
-}
-
-var hidePickedCards = function(type) {
-	pickedCards.forEach(function(card) {
-		card.classList.add( 'hidden' );
-	});
-	pickedCards.length = 0;
-	hiddenPairsCount++;
-	checkWin();
-};
-
-var getType = function(card) {
-	return card.getAttribute("class").slice(5, 11);
-}
-
-cards.forEach(function(card) {
-	card.addEventListener('click', function() {
-		if (pickedCards.length === 0) {
-			pickCard(this);
-		} else if (pickedCards.length === 1) {
-			pickCard(this);
-			if (pickedCards[0] === pickedCards[1]) {
-				closeCards();
-			} else if ( getType(pickedCards[0]) === getType(pickedCards[1]) && pickedCards[0] !== pickedCards[1]) {
-				setTimeout(hidePickedCards, 800);
-			} else {
-				setTimeout(closeCards, 800);
-			}
-		}
-	});
-});
-
-//shuffle array
-function shuffle(o) {
-	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-		return o;
-};
-
+game.init();
